74b8e0d8c7ba8e0fd7a8421f2225fd68cdafdf65
Alan Creech <acreech@ironspot.com>
2012-06-03 15:18:57 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ironspot::Application.config.secret_token = 'cced1ac030a746ce96134a1b1b0c57f4efdc68ffba3285e258fe3170579c64f425f760e55491487a6027f0d97d7103e40181e01cb7e2e6d4f175bedcaf895f73'
